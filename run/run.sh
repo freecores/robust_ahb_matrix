3680dcc1abd2f1d8f62de54653f89d5a9d6c2c79
eyalhoc <eyalhoc@086e94e0-a9ff-4bf6-93fe-78a523495658>
2011-06-13 20:52:31 +0000

    RobustVerilog version 1.4 compatible

--- run/run.sh
+++ run/run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-../../../robust -null
+../../../../robust -null
 if [ $? -eq 0 ];then
-  ROBUST=../../../robust
+  ROBUST=../../../../robust
 else
   echo "RobustVerilog warning: GUI version not supported - using non-GUI version robust-lite"
-  ROBUST=../../../robust-lite
+  ROBUST=../../../../robust-lite
 fi
 
-#$ROBUST ../src/base/ahb_matrix.v -od out -I ../src/gen -list list.txt -listpath -header -gui ${@}
-$ROBUST robust_ahb_matrix.pro -gui ${@}
+#$ROBUST src/base/ahb_matrix.v -od out -I ../src/gen -list list.txt -listpath -header -gui ${@}
+$ROBUST ../robust_ahb_matrix.pro -gui ${@}
